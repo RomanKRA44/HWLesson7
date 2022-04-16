c1d346edeb08fffcf6dbe84d1a76672c190bb2b9
RomanKRA44 <adm-nppl@yandex.ru>
2022-04-16 14:33:30 +0300

    Добавьте файлы проекта.

---
+++ HWLesson7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HWLesson7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int diag_arr = 0;
+            int[,] array = new int[2, 2];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = diag_arr + 1;
+                    Console.WriteLine($"{new string(' ', diag_arr)}{array[i, j]}");
+                    diag_arr++;
+                }
+            }
+        }
+    }
+}
